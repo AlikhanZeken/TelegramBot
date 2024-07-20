887f643673b92b43d2409b2d17ff58d5dbcaec1e
AlikhanZeken <zeken90@inbox.ru>
2024-07-20 16:06:08 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '7241648333:AAHD1BEsk1HC9xUOc7HOCfhe98TWW7GIlr8'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'тенге': 'KZT',
+}
